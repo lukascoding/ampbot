7d905b691a9dacdb8386e5db1b05f903e13ed6ab
Lukas <lukasm@my.com>
2017-08-30 14:45:25 +0200

    added updatePlaylistName

--- src/ampbot/ampbot/Lib/Spotify/Playlist.py
+++ src/ampbot/ampbot/Lib/Spotify/Playlist.py
@@ -1,6 +1,7 @@
 from Lib.Config import Parser
 import spotipy
 import spotipy.util as util
+import arrow
 
 def addTrack(id, config):
     if 'spotify:track:' in id:
@@ -11,3 +12,12 @@ def addTrack(id, config):
         sp.user_playlist_add_tracks(config.production.spotify.username, config.production.spotify.playlistId, temp)
     else:
         pass
+
+def updatePlaylistName(firstName, config):
+    local = arrow.utcnow().to('Europe/Berlin')
+    token = util.prompt_for_user_token(config.production.spotify.username, scope=config.production.spotify.scopes, client_id=config.production.spotify.clientId, client_secret=config.production.spotify.clientSecret, redirect_uri=config.production.spotify.redirectUri)
+    sp = spotipy.Spotify(auth=token)
+    sp.user_playlist_change_details(
+        config.production.spotify.username,
+        config.production.spotify.playlistId,
+        name='ampbot [last update {0} by {1}]'.format(local.format('YYYY-MM-DD'), firstName))
